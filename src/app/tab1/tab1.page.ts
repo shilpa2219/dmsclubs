c9010962babc08b0acf899ead1fbf4c3a3e31fcf
shilpa2219 <shilpams2219@gmail.com>
2020-11-26 14:15:13 +0530

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  user:any[]=[]
+  mobile:number
+  password:string
+  code:number
+  accept:string
 
-}
+  constructor(public navCtrl:NavController) {}
+  register(){
+    console.log("Mobile Number : ",this.mobile);
+    console.log("Password : ",this.password);
+    console.log("Recommendation Code : ",this.code);
+    console.log("Privacy Policy : ",this.accept);
+
+    }
+    
+    
+    
+    
+
+  }
+  
